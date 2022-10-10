701e0227d4c15298b09dfe6fca064ef84ab3dc71
amityadav0912 <86912765+amityadav0912@users.noreply.github.com>
2022-10-10 22:36:39 +0530

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 const menuToggle = document.querySelector("#menu-toggle");
 const activeElements = document.querySelectorAll(".active-element");
 
-menuToggle.onclick=function(){
+const nav = function(){
     for(let active=0; active<activeElements.length;active++)
     {
         activeElements[active].classList.toggle("active")
@@ -22,3 +22,17 @@ menuToggle.onclick=function(){
         item.style.transition=`all .5s linear ${index*0.3}s`;
     });
 };
+const navclose = function(){
+    for(let active=0; active<activeElements.length;active++)
+    {
+        activeElements[active].classList.remove("active")
+    }
+    const navBtnIcon = menuToggle.childNodes[1];
+    navBtnIcon.classList.remove('fa-times');
+    const pageBg = document.querySelector(".main-container");
+    setTimeout(() => {
+            pageBg.style.filter="blur(0px)";
+    }, 500);  
+};
+menuToggle.onclick=nav;
+document.querySelector(".main-container").onclick=navclose;
